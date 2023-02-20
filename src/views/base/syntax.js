dd034e50cd392a47ffac751b4d705a03ba7d9098
李智 <254738545@qq.com>
2023-02-20 22:11:47 +0800

    init

---
+++ src/views/base/syntax.js
@@ -0,0 +1,38 @@
+import logo from "../../logo.svg"
+import styles from './index.module.css'
+export default function Syntax() {
+  const name = "React";
+  const obj = {
+    firstName: 'li',
+    lastName: 'zhi'
+  }
+  function formatName() {
+    return obj.firstName + "." + obj.lastName;
+  }
+
+  const greet = <div>good</div>
+
+  const show = true;
+
+  const arr = ["吃饭", "学习", "工作", "睡觉"];
+  return (
+    <>
+      {/* 表达式 */}
+      <div>hello, {name}</div>
+      {/* 函数 */}
+      <div>{formatName(obj)}</div>
+      {/* jsx对象 */}
+      {greet}
+      {/* 条件语句 */}
+      {show ? greet : "全量"}
+      {show && greet}
+      {/* 数组 */}
+      <ul>
+        {arr.map((item, key) => (
+          <li key={key}>{item}</li>
+        ))}
+      </ul>
+      <img src={logo} className={styles.logo} alt="" />
+    </>
+  )
+}
